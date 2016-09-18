c512eb742eb378879adf6efa617283e55cba1f30
jeffreymei <mym226@nyu.edu>
2016-09-18 19:57:17 -0400

    fixed matrix multiplication in quad bowl

--- Project 1/Question1.py
+++ Project 1/Question1.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 from scipy import linalg
 import matplotlib.pyplot as plt
-
+import os 
 x_old = np.array([0.,0.]) # The value does not matter as long as abs(x_new - x_old) > precision
 x_new = np.array([5,3.1]) # The algorithm starts at x=6
 gamma = 0.01 # step size
@@ -21,9 +21,11 @@ def df2(x, n, sigma, u):
 
 	return f, result
 
+
+
 def df3 (x,A,b):
- 	f= 0.5*x.T*A*x-x.T*b
- 	result =A*x-b
+ 	f= 0.5*x.T.dot(A).dot(x)-x.T.dot(b)
+ 	result =A.dot(x)-b
 
  	return result
 
@@ -32,14 +34,24 @@ n = 1
 u = np.array([3.2,2])
 
 #sys.exit()
-steps = 0
+
 while np.sqrt(sum((x_new-x_old)**2)) > precision:
 	x_old = x_new
 	x_new = x_new -gamma * df2(x_old, n=n, sigma=sigma, u=u)[1]
-	steps += 1
+	
 print("The local minimum occurs at "+str(x_new))
 
 
 sys.exit()
 plt.plot(xs, df(xs))
 plt.show()
+
+
+#need to reinitialize x_old, x_new
+(gaussMean,gaussCov,quadBowlA,quadBowlb)  = getData()
+steps = 0
+while np.sqrt(sum((x_new-x_old)**2)) > precision:
+	x_old = x_new
+	x_new = x_new - gamma* df3(x_old, quadBowlA, quadBowlb)
+	steps+=1
+print("The local minimum occurs at "+str(x_new))
