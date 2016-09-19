a79c71c43ed6e75b02d32a3d4a90d7803eebca0f
jeffreymei <mym226@nyu.edu>
2016-09-18 20:42:54 -0400

    added optimal gamma values

--- Project 1/Question1.py
+++ Project 1/Question1.py
@@ -3,55 +3,47 @@ import scipy
 from scipy import linalg
 import matplotlib.pyplot as plt
 import os 
-x_old = np.array([0.,0.]) # The value does not matter as long as abs(x_new - x_old) > precision
-x_new = np.array([5,3.1]) # The algorithm starts at x=6
-gamma = 0.01 # step size
-precision = 0.00000001
-
+os.chdir("/home/jeffrey/6867_2016/Project 1/P1")
+from loadParametersP1 import getData
+from loadFittingDataP1 import getData as getFitData
 
 def df(x):
 	y = 4 * x**3 - 9 * x**2
 	return y
 
 def df2(x, n, sigma, u):
-	
 	f= -1./(np.sqrt((2.*np.pi)**n*linalg.det(sigma)))*np.exp(-0.5*(x-u).T.dot(linalg.inv(sigma)).dot((x-u)))
- 
  	result = -f*linalg.inv(sigma).dot((x-u))
-
-	return f, result
-
+	return result
 
 
 def df3 (x,A,b):
  	f= 0.5*x.T.dot(A).dot(x)-x.T.dot(b)
  	result =A.dot(x)-b
-
  	return result
 
-sigma = np.eye(2)
 n = 1
-u = np.array([3.2,2])
 
-#sys.exit()
+x_old = np.array([0.,0.]) # The value does not matter as long as abs(x_new - x_old) > precision
+x_new = np.array([8,12]) # The algorithm starts at x=6
+gamma = .01 # gamma should be >1000 for gaussian, <0.1 for quadBowl
+precision = 1e-5
 
-while np.sqrt(sum((x_new-x_old)**2)) > precision:
+(gaussMean,gaussCov,quadBowlA,quadBowlb)  = getData()
+count=0
+
+
+while np.sqrt(sum((x_new-x_old)**2)) > precision and count<10000:
 	x_old = x_new
-	x_new = x_new -gamma * df2(x_old, n=n, sigma=sigma, u=u)[1]
-	
+	x_new = x_new -gamma * df3(x_old, quadBowlA, quadBowlb)
 print("The local minimum occurs at "+str(x_new))
-
+# while np.sqrt(sum((x_new-x_old)**2)) > precision and count<10000:
+# 	x_old = x_new
+# 	x_new = x_new - gamma* df2(x_old, 1, gaussCov, gaussMean)
+# 	count+=1
+# print("The local minimum occurs at "+str(x_new))
 
 sys.exit()
-plt.plot(xs, df(xs))
-plt.show()
 
 
-#need to reinitialize x_old, x_new
-(gaussMean,gaussCov,quadBowlA,quadBowlb)  = getData()
 steps = 0
-while np.sqrt(sum((x_new-x_old)**2)) > precision:
-	x_old = x_new
-	x_new = x_new - gamma* df3(x_old, quadBowlA, quadBowlb)
-	steps+=1
-print("The local minimum occurs at "+str(x_new))
